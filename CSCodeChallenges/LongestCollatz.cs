73a146adbc487c314632c7235e1e90389758d6fb
Luke Kelley <lucas.j.kelley@gmail.com>
2023-03-01 22:39:02 -0600

    completed code challenges with whisky

---
+++ CSCodeChallenges/LongestCollatz.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSCodeChallenges
+{
+    internal class LongestCollatz
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * n - > n/2 (WHEN n is even use this)
+             * n - > 3n + 1 (WHEN n is odd use this)
+             * 
+             */
+
+            List<int> longCollatz = new List<int>() { };
+
+            int currentValue = 0;
+            int collatzMax = 0;
+            int collatzNum = 0;
+
+            for (int i = 1; i <= 1000; i++)
+            {
+                currentValue = i;
+                bool exit = false;
+                do
+                {
+                    longCollatz.Add(currentValue);
+
+                    if (currentValue == 1 || currentValue == 0)
+                    {
+                        exit = true;
+                    }
+                    if (currentValue % 2 == 0)
+                    {
+                        currentValue /= 2;
+
+                    }
+                    else if (exit != true)
+                    {
+                        currentValue = (currentValue * 3) + 1;
+                    }
+
+                } while (exit != true);
+
+                if (longCollatz.Count > collatzMax)
+                {
+                    collatzMax = longCollatz.Count;
+                    collatzNum = i;
+                }
+
+                //foreach (var item in longCollatz)
+                //{
+                //    Console.Write(item + "-> ");
+                //}
+
+                longCollatz.Clear();
+            }
+            Console.WriteLine($"The starting number of {collatzNum} produces the longest chain with {collatzMax} terms.");
+
+        }
+    }
+}
---
+++ CSCodeChallenges/TenDigitFibonacci.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSCodeChallenges
+{
+    internal class TenDigitFibonacci
+    {
+        static void Main(string[] args)
+        {
+            /*
+             *   Fn-1 + Fn-2, where F1 = 1, F2 = 1, F3 = 2, .... and F12 = 144.  F12 is the first term to contain 3 digits.
+             *   
+             *   What is the index of the first term in the Fibonacci sequence to contain 10 digits?
+             */
+            
+            int sum = 3;
+            int n1 = 1;
+            int n2 = 1;
+            int next = 0;
+
+            while (next <= 1_100_000_000)
+            {
+                next = n2 + n1;
+                if (next >= 1_000_000_000)
+                {
+                    Console.WriteLine($"The first 10 digit number in the Fibonacci Sequence is: {next:n0}.\nAnd it is in the {sum}th index.");
+                }
+                n1 = n2;
+                n2 = next;
+                sum++;
+            }
+            
+        }
+    }
+}
